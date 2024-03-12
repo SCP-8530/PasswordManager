f80a1f42e314d3e0c71145976847bcfb72adc407
Anno (Guillaume Paoli) <wdgaster.gtp@gmail.com>
2024-03-12 08:36:49 -0400

    serialisation

--- Client/src/main/java/Main.java
+++ Client/src/main/java/Main.java
@@ -1,3 +1,11 @@
+import com.google.gson.Gson;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Dictionary;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -10,6 +18,23 @@ public class Main {
         System.out.println(p2.getPassword());
         System.out.println(p3.getPassword());
         System.out.println(p1.estValide(p1.getPassword()));
+
+        Map<String,String> list_password = new HashMap<String,String>();
+
+        list_password.put("Facebook",p1.getPassword());
+        list_password.put("Google",p2.getPassword());
+        list_password.put("Twitter",p3.getPassword());
+
+        //json
+        try {
+            Gson gson = new Gson();
+            String json = gson.toJson(list_password, Map.class);
+            FileWriter writer = new FileWriter("list_password.json");
+            writer.write(json);
+            writer.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 }
